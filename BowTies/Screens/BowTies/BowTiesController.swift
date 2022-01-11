f0c689910e3a57a64418fc3b72f7725f08bd48d4
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2022-01-11 22:40:48 +0300

    Configured project

--- BowTies/Screens/BowTies/BowTiesController.swift
+++ BowTies/Screens/BowTies/BowTiesController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  BowTiesController.swift
 //  BowTies
 //
 //  Created by Stanislav Lemeshaev on 11.01.2022.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class BowTiesController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
-
-
 }
-
