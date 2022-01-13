a49cae88fda8e613301b9e8923a82e488ef1707c
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2022-01-13 17:50:17 +0300

    Added UI-items
    - Added UI-items on BowTiesView;
    - Connected UI-items to BowTiesController.

--- BowTies/Screens/BowTies/BowTiesController.swift
+++ BowTies/Screens/BowTies/BowTiesController.swift
@@ -9,8 +9,32 @@ import UIKit
 
 class BowTiesController: UIViewController {
     
+    // MARK: - IBOutlets
+    @IBOutlet private weak var segmentedControl: UISegmentedControl!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var ratingLabel: UILabel!
+    @IBOutlet private weak var timesWornLabel: UILabel!
+    @IBOutlet private weak var lastWornLabel: UILabel!
+    @IBOutlet private weak var favoriteLabel: UILabel!
+    @IBOutlet private weak var wearButton: UIButton!
+    @IBOutlet private weak var rateButton: UIButton!
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+    }
+    
+    // MARK: - IBActions
+    @IBAction private func segmentedControl(_ sender: UISegmentedControl) {
+        print(#function)
+    }
+    
+    @IBAction private func wear(_ sender: UIButton) {
+        print(#function)
+    }
+    
+    @IBAction private func rate(_ sender: UIButton) {
+        print(#function)
     }
 }
